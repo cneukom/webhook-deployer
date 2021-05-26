29aa1e6be068830da1613a07cb98e339c5af5111
Cédric Neukom <gitlab@cedl.ch>
2021-05-26 16:47:39 +0200

    Add HTTP Status codes

--- public/webhook.php
+++ public/webhook.php
@@ -5,36 +5,32 @@ $token = authToken();
 $configDir = __DIR__ . '/../config/';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    abort('Method not allowed');
+    abort('Method not allowed', 405);
 }
 
 if (!is_dir($configDir . $project)) {
-    abort('Unknown project ' . $project);
+    abort('Unknown project ' . $project, 404);
 }
 chdir($configDir . $project);
 
 if (!is_file('auth.php')) {
-    abort('No auth.php found for project');
+    abort('No auth.php found for project', 500);
 }
 $auth = require 'auth.php';
 
 if (!isset($auth['tokens'][$token])) {
-    abort('Invalid token');
+    abort('Invalid token', 403);
 }
 
 if (!in_array($stage, $auth['tokens'][$token])) {
-    abort('Stage not allowed for token');
+    abort('Stage not allowed for token', 403);
 }
 
 list($status, $output) = wrapExec(__DIR__ . '/../vendor/bin/dep deploy ' . $stage);
-if ($status !== 0) {
-    header('HTTP/1.1 500 Internal Server Error');
-}
-
 if (isset($auth['allowOutput']) && in_array($stage, $auth['allowOutput'])) {
-    echo $output;
-} else if ($status === 0) {
-    header('HTTP/1.1 204 No Content');
+    abort($output, $status === 0 ? 200 : 500);
+} else {
+    abort(null, $status === 0 ? 204 : 500);
 }
 
 
@@ -46,16 +42,29 @@ function authToken(): string
 
     $tokenPrefix = 'Bearer ';
     if (substr($_SERVER['HTTP_AUTHORIZATION'], 0, strlen($tokenPrefix)) !== $tokenPrefix) {
-        abort('Invalid token');
+        abort('Invalid token', 401);
     }
 
     return substr($_SERVER['HTTP_AUTHORIZATION'], strlen($tokenPrefix));
 }
 
-function abort($message)
+function abort(?string $responseContent, $code = 400)
 {
-    header('HTTP/1.1 400 Bad Request');
-    echo $message;
+    $statusCodes = [
+        200 => 'OK',
+        204 => 'No Content',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        500 => 'Internal Server Error',
+    ];
+
+    header("HTTP/1.1 $code ${statusCodes[$code]}");
+    if ($responseContent !== null) {
+        echo $responseContent;
+    }
     exit();
 }
 
